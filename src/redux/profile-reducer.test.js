e5a008f37227f43e3427ad3ee5bbcd7f447ec6a5
MirToykin <miroslav.toykin@gmail.com>
2020-03-15 01:49:18 +0300

    corrected profile-reducer test

--- src/redux/profile-reducer.test.js
+++ src/redux/profile-reducer.test.js
@@ -4,11 +4,11 @@ let state = {
   userProfile: null
 }
 
-it(`after setting of auth data id shouldn't be null`, () => {
+it(`after setting of useProfile it shouldn't be null`, () => {
   // start test data
   let action = setUserProfile({id: 1});
   // action
   let newState = profileReducer(state, action);
   // expectation
-  expect(newState.userProfile.id).toBe(1);
+  expect(newState.userProfile.id).toBe({id: 1});
 });
