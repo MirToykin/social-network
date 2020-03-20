5dff5d2fa52c790b30900ad13d2a38b90a7e0dab
MirToykin <miroslav.toykin@gmail.com>
2020-03-21 02:39:18 +0300

    added profile status simple tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
 import App from './App';
+import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
+import store from "./redux/redux-store";
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const div = document.createElement('div');
+  ReactDOM.render(<BrowserRouter><Provider store={store}><App /></Provider></BrowserRouter>, div)
+  ReactDOM.unmountComponentAtNode(div);
 });
