21e343b4942b9798070be84ada5a1dd43fdd8804
MirToykin <miroslav.toykin@gmail.com>
2020-03-24 23:46:39 +0300

    gh-pages deploy

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './redux/redux-store'
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
-ReactDOM.render(<BrowserRouter>
+ReactDOM.render(<HashRouter> // HashRouter для gh-pages, основной BrowserRouter
   <Provider store={store}>
     <App state={store.getState()}/>
   </Provider>
-</BrowserRouter>, document.getElementById('root'));
+</HashRouter>, document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
